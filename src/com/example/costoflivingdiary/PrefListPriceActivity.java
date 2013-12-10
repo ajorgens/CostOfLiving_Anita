3e4640e38511493419506ac5352ceb275f5ec5ff
ajorgens <anita.c.jorgensen@gmail.com>
2013-12-10 12:41:55 -0500

    show all prices for preferences

--- src/com/example/costoflivingdiary/PrefListPriceActivity.java
+++ src/com/example/costoflivingdiary/PrefListPriceActivity.java
@@ -2,6 +2,8 @@ package com.example.costoflivingdiary;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
 import android.app.ListActivity;
@@ -10,7 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class PrefListPriceActivity extends ListActivity {
-	
+	public TreeMap<String, TreeMap<String, String>> countryPrices = new TreeMap<String, TreeMap<String, String>>();
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,14 +22,62 @@ public class PrefListPriceActivity extends ListActivity {
 		if(bundle != null) {
 			int index = bundle.getInt(MainActivity.ITEM_INDEX);
 			ArrayList<String> prefStringList = new ArrayList<String>();
+			
 			for(int i = 0; i < MainActivity.PREF_LIST.size(); i++) {
 				//TODO get prices somehow
-				prefStringList.add("Price in " + MainActivity.PREF_LIST.get(i).getPreference() + ": ");
+				String country = MainActivity.PREF_LIST.get(i).getPreference();
+				String result = queryNumbeo(country.replace(" ", "+"));
+				TreeMap<String, String> current = getAveragePrices(result);
+				countryPrices.put(country, current);
+				ArrayList<CostOfLivingItem> items = MainActivity.LIST;
+				CostOfLivingItem item = items.get(index);
+				prefStringList.add("Price in " + country + ": " + current.get(item.getItem()));
 			}
 			ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                     R.layout.prefitem, R.id.prefEntry, prefStringList);
 			setListAdapter(adapter);
 		}
+	}
+	
+	private TreeMap<String, String> getAveragePrices(String result) {
+
+		String[] array = result.split(",\"item_name\":\"");
+		String[] averagePrice = result.split("\"average_price\":");
+		ArrayList<String> prices = new ArrayList<String>();
+		ArrayList<String> items = new ArrayList<String>();
+		TreeMap<String, String> averagePricePerItem = new TreeMap<String, String>();
+		for (String a : averagePrice) {
+			if (!a.startsWith("{")) {
+				String[] arr = a.split(",\"");
+				prices.add(arr[0]);
+			}
+		}
+		for (String s : array) {
+			if (!s.startsWith("{")) {
+				String[] item = s.split("\",\"");
+				items.add(item[0]);
+			}
+		}
+		for (int i = 0; i < items.size(); i++) {
+			averagePricePerItem.put(items.get(i), prices.get(i));
+		}
+		return averagePricePerItem;
+	}
+
+	private String queryNumbeo(String country) {
+		QueryNumbeo q = new QueryNumbeo();
+		q.country = country;
+		q.execute();
+		// have to wait for doInBackground to finish so q.results is no longer
+		// null
+		if(q.results == null) {
+			try {
+				q.get(1000, TimeUnit.MILLISECONDS);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return q.results;
 	}
     
 
